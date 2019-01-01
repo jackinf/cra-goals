fe5805f33fd7ae7cdbd5e1f015495a7c0ddbe65f
Jevgeni Rumjantsev <jrumjantsev@travix.com>
2019-01-01 14:50:12 +0100

    added action icons; added date formatting

--- src/goals/GoalList.component.jsx
+++ src/goals/GoalList.component.jsx
@@ -1,7 +1,10 @@
 import React, {Component} from 'react';
-import {getGoals} from './Goal.api';
-
 import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
+import moment from 'moment';
+import ViewIcon from '@material-ui/icons/Visibility';
+import EditIcon from '@material-ui/icons/Edit';
+import DeleteIcon from '@material-ui/icons/Delete';
 import { withStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
@@ -9,20 +12,30 @@ import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
-import { withRouter } from 'react-router-dom';
+import IconButton from '@material-ui/core/IconButton';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import {getGoals} from './Goal.api';
+
+const ITEM_HEIGHT = 48;
 
 const styles = theme => ({
   root: {
     width: '100%',
     marginTop: theme.spacing.unit * 3,
     overflowX: 'auto',
-  }
+  },
+  icon: {
+    margin: theme.spacing.unit,
+    fontSize: 24,
+  },
 });
 
 class GoalList extends Component {
   constructor(props) {
     super(props);
-    this.state = {paginatedList: {}};
+    this.state = {paginatedList: {}, actionMenuEl: null};
   }
 
   async componentDidMount() {
@@ -30,7 +43,24 @@ class GoalList extends Component {
     this.setState({paginatedList});
   }
 
+  handleOpenMenu = event => {
+    this.setState({ actionMenuEl: event.currentTarget });
+  };
+
+  handleCloseMenu = () => {
+    this.setState({ actionMenuEl: null });
+  };
+
+  viewItem = (id) => {
+    this.props.history.push(`/goals/${id}`);
+  };
+
+  deleteitem = (id) => {
+
+  };
+
   render() {
+    const actionMenuEl = this.state.actionMenuEl;
     const {classes} = this.props;
 
     return (
@@ -41,20 +71,55 @@ class GoalList extends Component {
             <TableRow>
               <TableCell>Title</TableCell>
               <TableCell align="right">Due</TableCell>
+              <TableCell align="right" />
             </TableRow>
           </TableHead>
           <TableBody>
             {this.state.paginatedList &&
             this.state.paginatedList.items &&
             this.state.paginatedList.items.map(row => {
+              const open = Boolean(actionMenuEl);
+
               return (
-                <TableRow key={row.id} onClick={e => {
-                  this.props.history.push('/goals/' + row.id);
-                }}>
+                <TableRow key={row.id}>
                   <TableCell component="th" scope="row">
                     {row.title}
                   </TableCell>
-                  <TableCell align="right">{row.due}</TableCell>
+                  <TableCell align="right">{moment(row.due).format("MMM Do YY")}</TableCell>
+                  <TableCell align="right">
+                    <IconButton
+                      aria-label="More"
+                      aria-owns={open ? 'long-menu' : undefined}
+                      aria-haspopup="true"
+                      onClick={this.handleOpenMenu}
+                    >
+                      <MoreVertIcon />
+                    </IconButton>
+                    <Menu
+                      anchorEl={actionMenuEl}
+                      open={open}
+                      onClose={this.handleCloseMenu}
+                      PaperProps={{
+                        style: {
+                          maxHeight: ITEM_HEIGHT * 4.5,
+                          width: 200,
+                        },
+                      }}
+                    >
+                      <MenuItem onClick={() => this.viewItem(row.id)}>
+                        <ViewIcon className={classes.icon} />
+                        View
+                      </MenuItem>
+                      <MenuItem onClick={this.handleCloseMenu}>
+                        <EditIcon className={classes.icon} />
+                        Edit
+                      </MenuItem>
+                      <MenuItem onClick={this.handleCloseMenu}>
+                        <DeleteIcon className={classes.icon} />
+                        Delete
+                      </MenuItem>
+                    </Menu>
+                  </TableCell>
                 </TableRow>
               );
             })}
